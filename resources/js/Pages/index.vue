01f314288bf3b40ba63204bd4d07cd0ef2d87fe9
mbs-code <mbscodec@gmail.com>
2023-08-13 19:15:58 +0900

    add: inertia vue

---
+++ resources/js/Pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <dev>
+        <Head title="ようこそ" />
+        <h1>ようこそ</h1>
+        <p>こんにちは {{ name }} さん。</p>
+    </dev>
+</template>
+
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ name: String })
+</script>
